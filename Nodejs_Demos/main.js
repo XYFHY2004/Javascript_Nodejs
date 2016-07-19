3a0b40542371061e74b316786d8c535080d86eb2
xyfhy <ljms8181@126.com>
2016-07-19 15:18:04 +0800

    move files to base folder

--- Nodejs_Demos/main.js
+++
@@ -1,6 +0,0 @@
-// 引入依赖模块
-var hello = require('./util/hello');
-
-console.log(hello.Hello());
-console.log(hello.Hello());
-console.log(hello.Hello());
--- Nodejs_Demos/util/hello.js
+++
@@ -1,9 +0,0 @@
-// console.log("Hello Nodejs");
-// var i = 0;
-let i = 0;
-function Hello(){
-	console.log("Hello Nodejs " + i);
-	i++;
-}
-// 导出公有方法
-exports.Hello = Hello;
